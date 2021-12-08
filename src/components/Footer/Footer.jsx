d9c4a52480fa2f7d187c6c5e72fc11bfed1f6b68
usmanmani0 <usmanmani010@gmail.com>
2021-12-08 18:45:30 +0500

    Reciving from data from Afzal

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import footer from "./footer.css";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import CashNowLogo from "../../assets/images/CashNowLogo.png";
 
 const Footer = () => {
-    return (
-        <div>
-            Footer
-        </div>
-    )
-}
+  return (
+    <Container className="footer_container" fluid>
+      <Container >
+        <Row>
+          <Col >
+            <span className="privacy_footer_txt">Privacy</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="footer_img_para">
+              <Image
+                className="pageT_icon img_cards img_footer"
+                src={CashNowLogo}
+                rounded
+              />
+              <p className="footer_para_txt">
+                Snelle verkoop is een proffesionele opkoper in Vlaanderen. We
+                helpen jou met een snelle verkoop van uw vastgoed (huis,
+                appartement, grond) Koopvoorstel nodig? Neem contact op. <br /> Ons
+                bedrijf is gelegen te ….. Tel: 0470 00 00 00, email:
+                info@snelleverkoop.be
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Webpagefive/WebPageFive.jsx
+++ src/components/Webpagefive/WebPageFive.jsx
@@ -1,11 +1,84 @@
-import React from 'react'
+import React from "react";
+import webpagefive from "./webpagefive.css";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import DollorIcon from "../../assets/images/DollorIcon.png";
+import LoopIcon from '../../assets/images/LoopIcon.png'
 
 const WebPageFive = () => {
-    return (
-        <div>
-            WebPageFive
-        </div>
-    )
-}
+  return (
+    <Container className="pg_five_container">
+      <Row>
+        <Col>
+          <div className="pg_five_img_para_wrapper">
+            <div>
+              <h3 className="pg_five_heading_txt">Gen makelaarscommissie</h3>{" "}
+              <br />
+              <p className="pageFour_para_txt_1">
+                Wij zijn GEEN vastgoedkantoor. Er zijn geen commissies
+                verschuldigd. GEEN openhuizendag, online publicatie of te koop
+                borden en stickers.
+              </p>
+            </div>
+            <Image className="pageT_icon img_cards" src={DollorIcon} rounded />
+          </div>
+        </Col>
+      </Row>
 
-export default WebPageFive
+      <Row>
+        <Col>
+          <div className="pageFour_img_para_wrapper">
+            <Image className="pageT_icon img_cards" src={DollorIcon} rounded />
+
+            <div>
+              <h3 className="pageFour_heading_txt">Snelle verkoop</h3> <br />
+              <p className="pageFour_para_txt_1">
+                De snelste weg om uw huis te verkopen in Vlaanderen! Gen
+                tijdsinvestering voor openhuizendag ect., directe verkoop!
+              </p>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <hr />
+        </Col>
+      </Row>
+
+      <Row>
+          <Col>
+          <div className="hs_search_form_wrapper_pg_five">
+              <p>Ontvang een koopvoorstel</p>
+
+              <form action="/action_page.php" className="inpt_form_hs">
+                <div className="input_group_pg_five">
+                  <input
+                    type="text"
+                    id="fname"
+                    name="fname"
+                    className="input1"
+                    placeholder="Straat"
+                  ></input>
+                  <input
+                    type="text"
+                    id="fname"
+                    name="fname"
+                    className="input2 input2_pg_five"
+                    placeholder="Postcode, Stad"
+                  ></input>
+                </div>
+
+                <input
+                  type="submit"
+                  value="Ontvang een bod >>"
+                  className="sb_btn_hs"
+                />
+              </form>
+            </div>
+          </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default WebPageFive;
